5af06e101fd7be8cf4b1f880a8c03ee7c6dca0c2
shreedhar desai <desaishreedhar09@gmail.com>
2019-10-03 20:43:37 +0530

    minor changes

--- controller/routes.js
+++ controller/routes.js
@@ -5,12 +5,9 @@ const mailer = require('../routes/nodemailer');
 const resetPassword = require('../routes/resetpassword');
 const middleware = require('../middleware/user');
 module.exports = (app) => {
-    //app.use('/api/user',user);
-    //app.use('/api/movie', Genre);
-    //app.use('/api/movie', movies);
+    
     app.use('/api/users', userRegistration);
     app.use('/api/auth', auth);
-    //app.use('/api/', fileupload);
     app.use('/api/', mailer);
     app.use('/api/', resetPassword);
     app.use(middleware);
--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const config = require('config');
 const port = process.env.PORT || 5000;
 
 const middleware = require('./middleware/user');
-//const Genre = require('./routes/genre.routes');
+
 const contact = require('./routes/contact.routes');
 const userRegistration = require('./routes/userregistration.routes');
 const auth = require('./auth/auth');
-//const fileupload = require('./routes/file.routes');
+
 const mailer = require('./routes/nodemailer');
 const resetPassword = require('./routes/resetpassword');
 app.use(express.json());
@@ -24,7 +24,7 @@ if(config.get('host.mail') === 'Development mode'){
 if(process.env.NODE_ENV === 'production'){
     console.log(`password: ${config.get('password')}`);
 }
-// app.use(express.urlencoded({extended:true}))
+
 app.use(middleware);
 mongoose
 .connect('mongodb://localhost/ECommerce', { useNewUrlParser: true })
@@ -33,17 +33,11 @@ mongoose
 
 
 app.use('/uploads', express.static('uploads'));
-// console.log(process);
-// console.log(`Production mode: ${process.env.NODE_ENV}`);
-// console.log(`development mode : ${app.set('env')}`);
-// console.log('mode:', config.get('host.mail'));
-//app.use('/api/user',user);
-//app.use('/api/movie', Genre);
-//app.use('/api/movie', movies);
+
 app.use('/api/users', userRegistration);
 app.use('/api/contact', contact);
 app.use('/api/auth', auth);
-//app.use('/api/', fileupload);
+
 app.use('/api/', mailer);
 app.use('/api/', resetPassword);
 app.listen(port,() => {console.log(`server working on port number ${port}`)});
